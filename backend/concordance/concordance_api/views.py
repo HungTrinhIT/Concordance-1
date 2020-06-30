e3eccd7f737e6d41ae11637941a2849c7fd0c688
Le Hoai Bao <lehoaibao081999@gmail.com>
2020-06-30 10:00:00 +0700

    backend

--- backend/concordance/concordance_api/views.py
+++ backend/concordance/concordance_api/views.py
@@ -8,7 +8,7 @@ from contextlib import closing
 from django.db import connection
 
 
-class EndataAPI(viewsets.ModelViewSet):
+class EndataListView(viewsets.ModelViewSet):
     serializer_class = EndataSerializer
     queryset = Endata.objects.all()
 
@@ -16,15 +16,25 @@ class EndataAPI(viewsets.ModelViewSet):
     def upload(self, request, format=None):
         data = smart_str(sub(b'\xef\xbb\xbf',  b'',    request.FILES['filename'].read(
         )), encoding='utf-8', strings_only=False, errors='strict').splitlines()
-        pattern = r'(ED|VD|KD|CD)(\d{6})(\d{2})(?:\t(\S+))(?:\t(\S+))(?:\t(\S+))(?:\t(\S+))(?:\t(\S+))(?:\t(\S+))(?:\t(\S+))(?:\t(\S+))(?:\t(\S+))'
+        pattern = r'(ED)(\d{6})(\d{2})(?:\t(\S+))(?:\t(\S+))(?:\t(\S+))(?:\t(\S+))(?:\t(\S+))(?:\t(\S+))(?:\t(\S+))(?:\t(\S+))(?:\t(\S+))'
         sql = 'INSERT INTO EnData VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
         params = []
         for line in data:
             field = findall(pattern, line)[0]
+            if len(field) != 12:
+                return Response("file format wrong",status=404)
             params.append(field)
         with closing(connection.cursor()) as cursor:
             cursor.executemany(sql, params)
         return Response(status=204)
+    
+    @action(detail=False, methods=['get'])
+    def sentence(self,request):
+        result = []
+        for i in range(1,10):
+            result.append(self.queryset.filter(sentence_id=i)) 
+        return Response(result,status=204)
+
 
 class VndataAPI(viewsets.ModelViewSet):
     serializer_class = VndataSerializer
@@ -39,6 +49,8 @@ class VndataAPI(viewsets.ModelViewSet):
         params = []
         for line in data:
             field = findall(pattern, line)[0]
+            if len(field) != 12:
+                return Response("file format wrong",status=404)
             params.append(field)
         with closing(connection.cursor()) as cursor:
             cursor.executemany(sql, params)
