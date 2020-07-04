bf3c8883eb02d4b3743d3de8ffcf72debdf61650
HungTrinh99 <minhhung.it99@gmail.com>
2020-07-04 15:40:18 +0700

    add controller

--- concordance-admin/admin-webapp/src/Screens/Homepage/Homepage.js
+++ concordance-admin/admin-webapp/src/Screens/Homepage/Homepage.js
@@ -1,37 +1,38 @@
 import React, { Component } from "react";
 import "./Homepage.css";
-import Controller from "../Components/Controller";
-import Table from "../Components/Table";
+import Controller from "../../Components/Controller";
+import Table from "../../Components/Table";
 import { connect } from "react-redux";
-import { createAciton } from "../Redux/Action/index";
-import EnData from "../EnData.json";
-import ViData from "../ViData.json";
-import { FETCH_EN_DATA, FETCH_VI_DATA } from "../Redux/Action/type";
-import { dataService } from "../Services";
+import { createAciton } from "../../Redux/Action/index";
+import EnData from "../../EnData.json";
+import ViData from "../../ViData.json";
+import { FETCH_EN_DATA, FETCH_VI_DATA } from "../../Redux/Action/type";
+import { dataService } from "../../Services/index";
 
 class Homepage extends Component {
   componentDidMount() {
     // Call API here(componentDidMount) to get the ViData and EngData, then those data will be stored in Redux store
     // After that, we will get ViData and EngData from store and pass those data to EngTable and ViTable
     // Get viData
-    dataService
-      .fetchLanguageData_pagination(this.props.pageNumber, "vnData")
-      .then((res) => {
-        this.props.dispatch(createAciton(FETCH_VI_DATA, res.data.results));
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-
+    // dataService
+    //   .fetchLanguageData_pagination(this.props.pageNumber, "vnData")
+    //   .then((res) => {
+    //     this.props.dispatch(createAciton(FETCH_VI_DATA, res.data.results));
+    //   })
+    //   .catch((err) => {
+    //     console.log(err.message);
+    //   });
+      this.props.dispatch(createAciton(FETCH_VI_DATA, ViData.results));
     // Get enData
-    dataService
-      .fetchLanguageData_pagination(this.props.pageNumber, "enData")
-      .then((res) => {
-        this.props.dispatch(createAciton(FETCH_EN_DATA, res.data.results));
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
+    // dataService
+    //   .fetchLanguageData_pagination(this.props.pageNumber, "enData")
+    //   .then((res) => {
+    //     this.props.dispatch(createAciton(FETCH_EN_DATA, res.data.results));
+    //   })
+    //   .catch((err) => {
+    //     console.log(err.message);
+    //   });
+      this.props.dispatch(createAciton(FETCH_EN_DATA, EnData.results));
   }
   render() {
     return (
