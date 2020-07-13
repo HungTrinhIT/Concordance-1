b9472b7fdf01fa8124f4ce235304b76a3fd556c2
HungTrinh99 <minhhung.it99@gmail.com>
2020-07-13 12:29:51 +0700

    fix: do not load data in home page

--- concordance-user/src/App.js
+++ concordance-user/src/App.js
@@ -7,7 +7,58 @@ import Help from "./Screens/Help";
 import Aboutus from "./Screens/Aboutus";
 import Statistics from "./Screens/Statistics";
 import Home from "./Screens/Home";
-export default class App extends Component {
+import { connect } from "react-redux";
+import { createAction } from "./Redux/Action";
+import { FETCH_VI_DATA, FETCH_EN_DATA } from "./Redux/Action/type";
+import { dataService } from "./Services";
+class App extends Component {
+  componentDidMount() {
+    // FETCH Vietnamese sentences
+    dataService
+      .fetchData_pagination(this.props.pageNumber, "vnsentence")
+      .then((response) => {
+        this.props.dispatch(createAction(FETCH_VI_DATA, response.data.results));
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+
+    // FETCH English sentences
+    dataService
+      .fetchData_pagination(this.props.pageNumber, "ensentence")
+      .then((response) => {
+        this.props.dispatch(createAction(FETCH_EN_DATA, response.data.results));
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.pageNumber !== nextProps.pageNumber) {
+      dataService
+        .fetchData_pagination(nextProps.pageNumber, "vnsentence")
+        .then((res) => {
+          this.props.dispatch(createAction(FETCH_VI_DATA, res.data.results));
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+
+      // Get enData
+      dataService
+        .fetchData_pagination(nextProps.pageNumber, "ensentence")
+        .then((res) => {
+          this.props.dispatch(createAction(FETCH_EN_DATA, res.data.results));
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+      return true;
+    }
+    return false;
+  }
+
   render() {
     return (
       <BrowserRouter>
@@ -23,3 +74,5 @@ export default class App extends Component {
     );
   }
 }
+
+export default connect()(App);
--- concordance-user/src/Screens/Home/index.js
+++ concordance-user/src/Screens/Home/index.js
@@ -3,60 +3,8 @@ import { connect } from "react-redux";
 import "./Home.css";
 import TableHome from "./TableHome";
 import Pagination from "../../Components/Pagination";
-import { dataService } from "../../Services";
-import { createAction } from "../../Redux/Action";
-import { FETCH_VI_DATA, FETCH_EN_DATA } from "../../Redux/Action/type";
 
 class Home extends Component {
-  componentDidMount() {
-    // FETCH Vietnamese sentences
-    console.log("didMount Home:",this.props.pageNumber);
-    dataService
-      .fetchData_pagination(this.props.pageNumber, "vnsentence")
-      .then((response) => {
-        this.props.dispatch(createAction(FETCH_VI_DATA, response.data.results));
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-
-    // FETCH English sentences
-    dataService
-      .fetchData_pagination(this.props.pageNumber, "ensentence")
-      .then((response) => {
-        this.props.dispatch(createAction(FETCH_EN_DATA, response.data.results));
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log("Update Home:", this.props.pageNumber, nextProps.pageNumber);
-    if (this.props.pageNumber !== nextProps.pageNumber) {
-      dataService
-        .fetchData_pagination(nextProps.pageNumber, "vnsentence")
-        .then((res) => {
-          this.props.dispatch(createAction(FETCH_VI_DATA, res.data.results));
-        })
-        .catch((err) => {
-          console.log(err.message);
-        });
-
-      // Get enData
-      dataService
-        .fetchData_pagination(nextProps.pageNumber, "ensentence")
-        .then((res) => {
-          this.props.dispatch(createAction(FETCH_EN_DATA, res.data.results));
-        })
-        .catch((err) => {
-          console.log(err.message);
-        });
-      return true;
-    }
-    return false;
-  }
-
   render() {
     return (
       <div className="home container">
@@ -65,7 +13,6 @@ class Home extends Component {
             <Pagination />
           </div>
         </div>
-
         <div className="home__table">
           <TableHome data={this.props.viData} />
           <TableHome data={this.props.enData} />
